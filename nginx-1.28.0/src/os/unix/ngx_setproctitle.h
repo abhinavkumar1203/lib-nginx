b83b2b76017ed32062adcf3e0bace345fb07d232
Unikraft <contact@unikraft.org>
2025-07-01 08:11:59 +0000

    feat: apply patch 0001 for setproctitle stub and 0002 for syscall stubs

---
+++ nginx-1.28.0/src/os/unix/ngx_setproctitle.h
@@ -0,0 +1,8 @@
+// nginx-1.28.0/src/os/unix/ngx_setproctitle.h
+#ifndef _NGX_SETPROCTITLE_H_INCLUDED_
+#define _NGX_SETPROCTITLE_H_INCLUDED_
+
+#define ngx_init_setproctitle(log)  /* no-op */
+#define ngx_setproctitle(title)     /* no-op */
+
+#endif /* _NGX_SETPROCTITLE_H_INCLUDED_ */
