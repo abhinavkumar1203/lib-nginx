5fabbe46c99f3a49d8e4888d6b9ea635ced6e4c4
Unikraft <contact@unikraft.org>
2025-07-01 08:14:49 +0000

    chore: ignore extracted nginx-1.28.0 source directory

--- nginx-1.28.0/src/os/unix/ngx_setproctitle.h
+++
@@ -1,8 +0,0 @@
-// nginx-1.28.0/src/os/unix/ngx_setproctitle.h
-#ifndef _NGX_SETPROCTITLE_H_INCLUDED_
-#define _NGX_SETPROCTITLE_H_INCLUDED_
-
-#define ngx_init_setproctitle(log)  /* no-op */
-#define ngx_setproctitle(title)     /* no-op */
-
-#endif /* _NGX_SETPROCTITLE_H_INCLUDED_ */
